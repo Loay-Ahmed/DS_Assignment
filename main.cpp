3cd6904deb52939f9edb496212aca1f03e6185c1
HP USER <loayabelgendy@gmail.com>
2024-04-23 19:14:21 +0200

    Added readFromFile function in main and students.txt

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
 #include "iostream"
+#include "fstream"
 
 using namespace std;
 
+string readFromFile(basic_ifstream<char> &file);
+
 int main() {
+    ifstream file("D:\\Gam3a\\DataStructures\\DS_Assignment\\students.txt");
+    if (!file.is_open()) {
+        cerr << "Can't open the file\n";
+        return -1;
+    }
+    readFromFile(file);
     return 0;
+}
+
+string readFromFile(basic_ifstream<char> &file) {
+    string line, name, id;
+    int n;
+    double gpa;
+    (file >> n) ? cout << n << endl : cout << "Can't read double";
+    for (int i = 0; i < n; i++) {
+        getline(file, name);
+        cout << name << " ";
+        getline(file, id);
+        cout << id << " ";
+        (file >> gpa) ? cout << gpa : cout << "Can't read double";
+        cout << endl;
+    }
+    return "";
 }
