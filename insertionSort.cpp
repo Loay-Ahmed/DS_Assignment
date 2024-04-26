50c2327b8b061f1ea90f8a9279cf986bf1529e1a
Nour_mohamed <148482343+NourMohamed6@users.noreply.github.com>
2024-04-26 14:36:52 +0300

    Update insertionSort.cpp

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 
 template<class T>
-void InsertionSort(T arr[], int, int n) {
-    for (int i = 1; i < n; i++) {
+void InsertionSort(T arr[], int s, int n) {
+    for (int i = s+1; i < n; i++) {
         T temp = arr[i];
         for (int j = i - 1; j >= 0; j--) {
             if (temp < arr[j]) {
-                swap(arr[(i - (i - j)) + 1], arr[j]);
+                swap(arr[j + 1], arr[j]);
             }
         }
     }
