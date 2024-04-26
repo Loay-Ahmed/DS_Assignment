d7e78760827305772b98e2db058da25698f4d2a8
Nour_mohamed <148482343+NourMohamed6@users.noreply.github.com>
2024-04-26 20:20:10 +0300

    Update CircularLinkedList.h

--- CircularLinkedList.h
+++ CircularLinkedList.h
@@ -1,48 +1,40 @@
-#include <string>
 using namespace std;
+#include <string>
+
 
 template<typename T>
-class CircularLinkedList {
 
-    struct cirqNode {
-        T val;
-        cirqNode *next;
-    };
 
-    cirqNode *tail;
-    int length;
+
+
+class CircularLinkedList
+{
+
+	struct cirqNode
+	{
+		T val;
+		cirqNode* next;
+	};
+
+	cirqNode* tail;
+	int length;
 
 public:
-    //Not Tested Yet
-    CircularLinkedList();
-
-    void insertAtHead(const T &element);
-
-    void insertAtTail(const T &element);
-
-    void insertAt(const T &element, int index);
-
-    void removeAtHead();
-
-    void removeAtTail();
-
-    void removeAt(int index);
-
-    T retrieveAt(int index);
-
-    void replaceAt(const T &element, int index);
-
-    bool isExist(const T &element);
-
-    bool isItemAtEqual(const T &element, int index);
-
-    void swap(int firstItemIdx, int secondItemIdx);
-
-    bool isEmpty();
-
-    int linkedListSize();
-
-    void clear();
-
-    void print();
+	//Not Tested Yet
+	CircularLinkedList();
+	void insertAtHead(const T& element);
+	void insertAtTail(const T& element);
+	void insertAt(const T& element, int index);
+	void removeAtHead();
+	void removeAtTail();
+	void removeAt(int index);
+	T retrieveAt(int index);
+	void replaceAt(const T& element, int index);
+	bool isExist(const T& element);
+	bool isItemAtEqual(const T& element, int index);
+	void swap(int firstItemIdx, int secondItemIdx);
+	bool isEmpty();
+	int linkedListSize();
+	void clear();
+	void print();
 };
